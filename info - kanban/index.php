857239ee6815f9fd019d81925cb5ef3110fbcf97
Christian Cuevas <ccpotential@gmail.com>
2018-03-14 21:56:05 -0700

    more stuff
    Clark was here :V

--- info - kanban/index.php
+++ info - kanban/index.php
@@ -1,8 +1,28 @@
-<?php
+<!DOCTYPE html>
+<html>
 
-  session_start();
-  if (!$_SESSION["user_data"]) {
-      header("Location: ./html/login.html");
-  }
+<head>
+  <title>Kanban</title>
 
-?>
+  <!-- Meta -->
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+  <!-- Include PHP -->
+  <?php
+    $access_level = 2;
+    include "./php/login.php";
+  ?>
+  <!-- *** -->
+
+  <!-- Import JS -->
+  <script src="./js/lib/muuri.min.js"></script>
+  <!-- *** -->
+
+</head>
+
+<body>
+
+</body>
+
+</html>
