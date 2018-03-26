b3dd4c8df15fa13ff3d069a1e9098c84925dba9a
Christian Cuevas <ccpotential@gmail.com>
2018-03-25 19:27:11 -0700

    redoing everything >_>
    Clark was here :V

---
+++ info - kanban/index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+
+  <!-- Setup the icon for the page -->
+  <link rel="icon" href="./assets/icon.png">
+
+  <title>Potential - Home</title>
+
+  <!-- Meta -->
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+  <!-- CSS -->
+  <link rel="stylesheet" href="./css/gui.css">
+
+  <!-- Javascript -->
+  <script src="./js/gui.js"></script>
+
+</head>
+
+<body>
+
+  <!-- Navigation Bar -->
+  <div class="nav">
+    <div class="menu">
+      <span class="title">
+        Metropoli2Go
+      </span>
+      <span class="user selectable" id="user">
+        ClarkThyLord
+      </span>
+    </div>
+    <div class="categories">
+      <div class="prev selectable">
+        &#10094;
+      </div>
+      <div class="next selectable">
+        &#10095;
+      </div>
+      <div class="category selectable choosen">
+        A
+      </div>
+      <div class="category selectable">
+        B
+      </div>
+      <div class="category selectable">
+        C
+      </div>
+      <div class="category selectable">
+        Add +
+      </div>
+    </div>
+  </div>
+
+  <!-- Workspace -->
+  <div>
+    <!-- Columns -->
+    <div class="">
+
+    </div>
+
+    <!-- Rows -->
+    <div class="">
+
+    </div>
+  </div>
+
+</body>
+
+</html>
