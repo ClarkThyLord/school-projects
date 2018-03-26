3ac2b10400bbc32eab34d334f0dacc5a9f21b33d
Christian Cuevas <ccpotential@gmail.com>
2018-03-25 21:37:47 -0700

    some more work
    Clark was here :V

--- info - kanban/index.php
+++ info - kanban/index.php
@@ -13,9 +13,14 @@
   <meta name="viewport" content="width=device-width, initial-scale=1" />
 
   <!-- CSS -->
+  <link rel="stylesheet" href="./css/lib/jquery-ui.min.css">
+  <link rel="stylesheet" href="./css/lib/jquery-ui-theme.css.css">
   <link rel="stylesheet" href="./css/gui.css">
 
   <!-- Javascript -->
+  <script src="./js/lib/jquery-3.3.1.min.js"></script>
+  <script src="./js/lib/jquery-ui.min.js"></script>
+  <script src="./js/lib/jquery-ui-touch-punch.min.js"></script>
   <script src="./js/gui.js"></script>
 
 </head>
@@ -24,6 +29,7 @@
 
   <!-- Navigation Bar -->
   <div class="nav">
+    <!-- Menu -->
     <div class="menu">
       <span class="title">
         Metropoli2Go
@@ -32,13 +38,14 @@
         ClarkThyLord
       </span>
     </div>
+    <!-- Categories -->
     <div class="categories">
-      <div class="prev selectable">
+      <a class="prev selectable">
         &#10094;
-      </div>
-      <div class="next selectable">
+      </a>
+      <a class="next selectable">
         &#10095;
-      </div>
+      </a>
       <div class="category selectable choosen">
         A
       </div>
@@ -49,20 +56,39 @@
         C
       </div>
       <div class="category selectable">
+        ...
+      </div>
+      <div class="category selectable">
+        &#9881;
+      </div>
+      <div class="category selectable">
         Add +
       </div>
     </div>
   </div>
 
   <!-- Workspace -->
-  <div>
-    <!-- Columns -->
-    <div class="">
-
+  <div class="workspace">
+    <!-- Toolbar -->
+    <div class="toolbar">
+      <label>
+        Search:
+        <input type="text" placeholder="Search term..." />
+      </label>
+      <label>
+        Filter:
+        <input type="text" placeholder="Filter by..." />
+      </label>
+      <span class="tool selectable">
+        &#9881;
+      </span>
+      <span class="tool selectable">
+        Add +
+      </span>
     </div>
 
-    <!-- Rows -->
-    <div class="">
+    <!-- Kanban -->
+    <div class="kanban">
 
     </div>
   </div>
