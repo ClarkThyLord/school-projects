289b850a5604752f3e433ea2c442feda35ea15e7
Christian Cuevas <ccpotential@gmail.com>
2018-05-02 22:02:46 -0700

    to spanish
    Clark was here :V

--- info - escalante guerra/index.php
+++ info - escalante guerra/index.php
@@ -19,15 +19,13 @@
 	<form class="form-signin">
 		<img class="mb-4" src="./assets/escalante_guerra_logo.jpg" alt="" width="125" height="125">
 		<h1 class="h3 mb-3 font-weight-normal">Please Sign In</h1>
-		<label for="inputEmail" class="sr-only">Email address...</label>
-		<input type="email" id="inputEmail" class="form-control" placeholder="Email address..." required autofocus>
-		<label for="inputPassword" class="sr-only">Password...</label>
-		<input type="password" id="inputPassword" class="form-control" placeholder="Password..." required>
-		<div class="checkbox mb-3">
-			<label>
-        <input type="checkbox" value="remember-me"> Remember me
-      </label>
-		</div>
+
+		<label for="inputEmail" class="sr-only">Insertar un correo electrónico válido</label>
+		<input type="email" id="inputEmail" class="form-control" placeholder="Correo electrónico..." required autofocus>
+
+		<label for="inputPassword" class="sr-only">Insertar contraseña válida</label>
+		<input type="password" id="inputPassword" class="form-control" placeholder="Contraseña..." required>
+
 		<button class="btn btn-lg btn-primary btn-block" type="submit">Sign In</button>
 	</form>
 </body>
