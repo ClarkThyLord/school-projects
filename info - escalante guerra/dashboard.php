d6456da0f31991c1d61c058c6afb5a3576884518
Christian Cuevas <ccpotential@gmail.com>
2018-05-02 22:05:31 -0700

    spanish titles
    Clark was here :V

--- info - escalante guerra/dashboard.php
+++ info - escalante guerra/dashboard.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <title>EG - Dashboard</title>
+    <title>EG - Tablero</title>
     <link rel="icon" href="./assets/escalante_guerra_logo.jpg">
 
 		<!-- CSS Libraries -->
