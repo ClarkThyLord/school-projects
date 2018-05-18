d5a55018ac8c7e057956e875bb80f616706a0dc2
Christian Cuevas <ccpotential@gmail.com>
2018-05-17 21:57:23 -0700

    refresh button!
    Clark was here ;V

--- info - escalante guerra/dashboard.php
+++ info - escalante guerra/dashboard.php
@@ -103,6 +103,12 @@
       <main role="main" class="page-content col-md-9 ml-sm-auto col-lg-10 px-4" id="desk">
         <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
           <h1 class="h2">Escritorio</h1>
+
+					<div class="btn-toolbar mb-2 mb-md-0">
+            <div class="btn-group mr-2">
+              <button onclick="content_refresh('desk');" class="btn btn-sm btn-outline-secondary">&#8635; Refrescar</button>
+            </div>
+          </div>
         </div>
 
         <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center">
