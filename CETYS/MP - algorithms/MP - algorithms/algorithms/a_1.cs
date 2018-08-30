8f1c6b3ecb0977eca557038d144e1386c4bd33d8
Christian Cuevas <ccpotential@gmail.com>
2018-08-30 13:15:41 -0700

    C#
    Clark was here :V

---
+++ CETYS/MP - algorithms/MP - algorithms/algorithms/a_1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MP___algorithms.algorithms
+{
+    class a_1
+    {
+        static public void run ()
+        {
+            Console.WriteLine("What up!");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ CETYS/MP - algorithms/MP - algorithms/main.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MP___algorithms
+{
+    class main
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+
+            algorithms.a_1.run();
+        }
+    }
+}
