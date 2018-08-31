af33b6f9d738194da0cf34fbee9d66d7947cf1e8
Christian Cuevas <ccpotential@gmail.com>
2018-08-31 13:38:38 -0700

    >_>
    Clark was here :V

--- CETYS/MP - algorithms/MP - algorithms/main.cs
+++ CETYS/MP - algorithms/MP - algorithms/main.cs
@@ -1,39 +1,65 @@
 ﻿using System;
-using System.IO;
-using System.Reflection;
-using MP___algorithms.commands.algorithms;
+using System.Collections.Generic;
 
 namespace MP___algorithms
 {
     class main
     {
-        static public string source_path = Directory.GetParent(Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName).FullName;
+        static public Dictionary<string, Func<string>> commands = new Dictionary<string, Func<string>>
+        {
+            {
+                "?",
+                help
+            },
+            {
+                "salir",
+                exit
+            }
+        };
 
         static void Main(string[] args)
         {
-            Type[] types = Assembly.GetExecutingAssembly().GetTypes();
-            foreach (Type type in types)
-            {
-                Console.WriteLine("{0}:", type.FullName.ToString());
-            }
-
             Console.WriteLine("Hola mundo!\n¿Qué te gustaría que haga hoy?");
             while (true)
             {
                 string answer = Console.ReadLine().ToLower();
-                switch (answer)
+
+                //switch (answer)
+                //{
+                //    case "?":
+                //        Console.WriteLine("Menú de Ayuda:\n**************\n?   : Ayuda\n\nsalir   :   Salir del programa");
+                //        break;
+                //    case "salir":
+                //        Console.WriteLine("¡Adiós!");
+                //        return;
+                //    default:
+                //        Console.WriteLine("No entendí del todo, intente de nuevo...");
+                //        break;
+                //}
+
+                if (commands.ContainsKey(answer))
                 {
-                    case "?":
-                        Console.WriteLine("Menú de Ayuda:\n**************\n?   : Ayuda\n\nsalir   :   Salir del programa");
-                        break;
-                    case "salir":
-                        Console.WriteLine("¡Adiós!");
-                        return;
-                    default:
-                        Console.WriteLine("No entendí del todo, intente de nuevo...");
-                        break;
+                    commands[answer]();
+                }
+                else
+                {
+                    Console.WriteLine("No entendí del todo, intente de nuevo...");
                 }
             }
+        }
+
+        static public string help()
+        {
+            Console.WriteLine("Menú de Ayuda:\n**************\n?   : Ayuda\n\nsalir   :   Salir del programa");
+
+            return "1";
+        }
+
+        static public string exit()
+        {
+            System.Environment.Exit(1);
+
+            return "1";
         }
     }
 }
