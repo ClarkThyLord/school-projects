a47065c75394f0849f3f2c6e786d12e0470cd8bd
Christian Cuevas <ccpotential@gmail.com>
2018-08-30 22:01:35 -0700

    reading from source and assembly
    Clark was here :V

--- CETYS/MP - algorithms/MP - algorithms/main.cs
+++ CETYS/MP - algorithms/MP - algorithms/main.cs
@@ -6,8 +6,24 @@ namespace MP___algorithms
 {
     class main
     {
+        static public string source_path = Directory.GetParent(Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName).FullName;
+
         static void Main(string[] args)
         {
+            string[] files = Directory.GetFiles(source_path + "/commands/");
+            string[] directories = Directory.GetDirectories(source_path + "/commands/");
+
+            foreach (string directory in directories)
+            {
+                Console.WriteLine(directory);
+            }
+
+            Assembly mscorlib = typeof(string).Assembly;
+            foreach (Type type in mscorlib.GetTypes())
+            {
+                Console.WriteLine(type.FullName);
+            }
+
             Console.WriteLine("Hola mundo!\n¿Qué te gustaría que haga hoy?");
             while (true)
             {
