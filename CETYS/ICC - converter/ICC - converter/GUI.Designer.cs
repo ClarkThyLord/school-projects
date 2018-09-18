1c89774ecca4756240fd89b012ee72035b684952
Christian Cuevas <ccpotential@gmail.com>
2018-09-17 23:00:51 -0700

    file handeling
    Clark was here :V

--- CETYS/ICC - converter/ICC - converter/GUI.Designer.cs
+++ CETYS/ICC - converter/ICC - converter/GUI.Designer.cs
@@ -31,12 +31,13 @@ namespace ICC___converter
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(GUI));
             this.header = new System.Windows.Forms.Label();
             this.input_gui = new System.Windows.Forms.TextBox();
-            this.result_gui = new System.Windows.Forms.TextBox();
-            this.convertir_gui = new System.Windows.Forms.Button();
+            this.output_gui = new System.Windows.Forms.TextBox();
+            this.convert_gui = new System.Windows.Forms.Button();
             this.progress_gui = new System.Windows.Forms.ProgressBar();
-            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.file_dialog = new System.Windows.Forms.OpenFileDialog();
             this.file_gui = new System.Windows.Forms.Button();
             this.type_gui = new System.Windows.Forms.ComboBox();
+            this.file_name_gui = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // header
@@ -61,26 +62,28 @@ namespace ICC___converter
             this.input_gui.Name = "input_gui";
             this.input_gui.Size = new System.Drawing.Size(700, 125);
             this.input_gui.TabIndex = 4;
+            this.input_gui.TextChanged += new System.EventHandler(this.input);
             // 
-            // result_gui
+            // output_gui
             // 
-            this.result_gui.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            this.output_gui.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.result_gui.Location = new System.Drawing.Point(18, 174);
-            this.result_gui.Multiline = true;
-            this.result_gui.Name = "result_gui";
-            this.result_gui.Size = new System.Drawing.Size(700, 125);
-            this.result_gui.TabIndex = 5;
+            this.output_gui.Location = new System.Drawing.Point(18, 174);
+            this.output_gui.Multiline = true;
+            this.output_gui.Name = "output_gui";
+            this.output_gui.ReadOnly = true;
+            this.output_gui.Size = new System.Drawing.Size(700, 125);
+            this.output_gui.TabIndex = 5;
             // 
-            // convertir_gui
+            // convert_gui
             // 
-            this.convertir_gui.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.convertir_gui.Location = new System.Drawing.Point(618, 306);
-            this.convertir_gui.Name = "convertir_gui";
-            this.convertir_gui.Size = new System.Drawing.Size(100, 35);
-            this.convertir_gui.TabIndex = 6;
-            this.convertir_gui.Text = "Convertir";
-            this.convertir_gui.UseVisualStyleBackColor = true;
+            this.convert_gui.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.convert_gui.Location = new System.Drawing.Point(618, 306);
+            this.convert_gui.Name = "convert_gui";
+            this.convert_gui.Size = new System.Drawing.Size(100, 35);
+            this.convert_gui.TabIndex = 6;
+            this.convert_gui.Text = "Convertir";
+            this.convert_gui.UseVisualStyleBackColor = true;
             // 
             // progress_gui
             // 
@@ -91,19 +94,20 @@ namespace ICC___converter
             this.progress_gui.Size = new System.Drawing.Size(392, 35);
             this.progress_gui.TabIndex = 7;
             // 
-            // openFileDialog1
+            // file_dialog
             // 
-            this.openFileDialog1.FileName = "openFileDialog1";
+            this.file_dialog.FileName = "file_dialog";
             // 
             // file_gui
             // 
             this.file_gui.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.file_gui.Location = new System.Drawing.Point(643, 9);
+            this.file_gui.Location = new System.Drawing.Point(547, 9);
             this.file_gui.Name = "file_gui";
-            this.file_gui.Size = new System.Drawing.Size(75, 28);
+            this.file_gui.Size = new System.Drawing.Size(171, 28);
             this.file_gui.TabIndex = 8;
-            this.file_gui.Text = "Archivo...";
+            this.file_gui.Text = "Abrir documento...";
             this.file_gui.UseVisualStyleBackColor = true;
+            this.file_gui.Click += new System.EventHandler(this.file_chooser);
             // 
             // type_gui
             // 
@@ -117,6 +121,19 @@ namespace ICC___converter
             this.type_gui.Name = "type_gui";
             this.type_gui.Size = new System.Drawing.Size(196, 24);
             this.type_gui.TabIndex = 9;
+            this.type_gui.Text = "Convertir a...";
+            // 
+            // file_name_gui
+            // 
+            this.file_name_gui.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.file_name_gui.AutoSize = true;
+            this.file_name_gui.BackColor = System.Drawing.SystemColors.Control;
+            this.file_name_gui.Location = new System.Drawing.Point(291, 9);
+            this.file_name_gui.MinimumSize = new System.Drawing.Size(250, 28);
+            this.file_name_gui.Name = "file_name_gui";
+            this.file_name_gui.Size = new System.Drawing.Size(250, 28);
+            this.file_name_gui.TabIndex = 10;
+            this.file_name_gui.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             // 
             // GUI
             // 
@@ -125,11 +142,12 @@ namespace ICC___converter
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Control;
             this.ClientSize = new System.Drawing.Size(732, 353);
+            this.Controls.Add(this.file_name_gui);
             this.Controls.Add(this.type_gui);
             this.Controls.Add(this.file_gui);
             this.Controls.Add(this.progress_gui);
-            this.Controls.Add(this.convertir_gui);
-            this.Controls.Add(this.result_gui);
+            this.Controls.Add(this.convert_gui);
+            this.Controls.Add(this.output_gui);
             this.Controls.Add(this.input_gui);
             this.Controls.Add(this.header);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
@@ -137,6 +155,8 @@ namespace ICC___converter
             this.Name = "GUI";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "Convertidor de Base";
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.file_drag_drop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.file_drag_enter);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -145,12 +165,13 @@ namespace ICC___converter
         #endregion
         private System.Windows.Forms.Label header;
         private System.Windows.Forms.TextBox input_gui;
-        private System.Windows.Forms.TextBox result_gui;
-        private System.Windows.Forms.Button convertir_gui;
+        private System.Windows.Forms.TextBox output_gui;
+        private System.Windows.Forms.Button convert_gui;
         private System.Windows.Forms.ProgressBar progress_gui;
-        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.OpenFileDialog file_dialog;
         private System.Windows.Forms.Button file_gui;
         private System.Windows.Forms.ComboBox type_gui;
+        private System.Windows.Forms.Label file_name_gui;
     }
 }
 
--- CETYS/ICC - converter/ICC - converter/GUI.cs
+++ CETYS/ICC - converter/ICC - converter/GUI.cs
@@ -19,5 +19,43 @@ namespace ICC___converter
         {
             InitializeComponent();
         }
+
+        private void input(object sender, EventArgs e)
+        {
+
+        }
+
+        private void file_open()
+        {
+
+        }
+
+        private void file_chooser(object sender, EventArgs e)
+        {
+            if (this.file_dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                System.IO.StreamReader sr = new System.IO.StreamReader(this.file_dialog.FileName);
+                
+                this.file_name_gui.Text = sr.ReadToEnd();
+
+                sr.Close();
+            }
+        }
+
+        private void file_drag_enter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.Copy;
+            }
+        }
+
+        private void file_drag_drop(object sender, DragEventArgs e)
+        {
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            foreach (string file in files) {
+                this.file_name_gui.Text += file.ToString();
+            }
+        }
     }
 }
