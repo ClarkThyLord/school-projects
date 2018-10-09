babf95a943a3300e08c528f91ad640389b8e46ad
Christian Cuevas <ccpotential@gmail.com>
2018-10-09 09:58:45 -0700

    the most useless exam I've ever taken in my life :v
    Clark was here :V

---
+++ CETYS/EXAM - pares/EXAM - pares/Program.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EXAM___pares
+{
+    class Program
+    {
+        public static List<int> tipos_de_billetes = new List<int> {
+            {
+                1
+            },
+            {
+                5
+            },
+            {
+                10
+            },
+            {
+                20
+            },
+            {
+                50
+            },
+            {
+                100
+            }
+        };
+
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                // INICIO
+                Console.Clear();
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("Christian Cuevas T029794 y Jorge Velarde T030681");
+                Console.WriteLine("------------------------------------------------");
+                Console.WriteLine("*****************");
+                Console.WriteLine("Cajero Automático V2.0");
+                Console.WriteLine("*****************\n\n");
+
+
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("Inserte una cantidad redondeada: (mayor o igual a $15 - menor o igual a $100,000)");
+                BigInteger cantidad = new BigInteger();
+                while (true)
+                {
+                    while (!BigInteger.TryParse(Console.ReadLine(), out cantidad))
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Por favor ingrese un número real...");
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        continue;
+                    }
+
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    if (cantidad < 15)
+                    {
+                        Console.WriteLine("Por favor ingrese un número mayor o igual a 15...");
+                    }
+                    else if (cantidad > 100000)
+                    {
+                        Console.WriteLine("Por favor ingrese un número menor o igual a 100,000...");
+                    }
+                    else
+                    {
+                        break;
+                    }
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                }
+
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine("\n\n");
+
+                // PROCESSO
+
+                int diferencia = 1;
+                BigInteger temp = cantidad;
+                string combinacion = "";
+                while (true)
+                {
+                    temp = cantidad;
+                    combinacion += String.Format("Posible combinación #{0}:", diferencia);
+                    foreach (int billete in tipos_de_billetes.Reverse<int>())
+                    {
+                        if (billete == 1)
+                        {
+                            BigInteger denominador = (temp / billete);
+                            if (denominador > 0)
+                            {
+                                combinacion += "\n" + String.Format("{1} billetes de ${0}", billete, denominador);
+
+                                temp -= denominador * billete;
+                                if (temp <= 0) break;
+                            }
+                        }
+                        else
+                        {
+                            BigInteger denominador = (temp / billete) - diferencia;
+                            if (denominador > 0)
+                            {
+                                combinacion += "\n" + String.Format("{1} billetes de ${0}", billete, denominador);
+
+                                temp -= denominador * billete;
+                                if (temp <= 0) break;
+                            }
+                        }
+                    }
+
+                    combinacion += "\n\n";
+                    diferencia++;
+                    if (diferencia == 4) break;
+                }
+
+                Console.WriteLine(combinacion);
+
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine("\n{0} escrito en Español con amor ;)\n\n", cantidad);
+
+                //FIN
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("¿Continuar? (y/n)");
+                string ans = Console.ReadLine().ToLower();
+                if (ans == "n")
+                {
+                    return;
+                }
+            }
+        }
+    }
+}
--- CETYS/MP - group solution/MP - group solution/main.cs
+++ CETYS/MP - group solution/MP - group solution/main.cs
@@ -14,81 +14,80 @@ namespace MP___group_solution
             int type = 0;
             while (true)
             {
-                type = ran.Next(1, 10);
+                type = ran.Next(2, 8);
                 
                 Console.Clear();
                 Console.ForegroundColor = ConsoleColor.White;
+                
+                Console.WriteLine("RESUELVA PARA UN POLÍGONO DE {0} LADOS", type);
+                
+                Console.ForegroundColor = ConsoleColor.Magenta;
 
-                if (type <= 5)
+                double lado_normal = 0;
+                List<double> lados = new List<double>();
+                double apotema = 0;
+                if (type < 5)
                 {
-                    Console.WriteLine("RESUELVA PARA UN POLÍGONO DE DOS LADOS");
-
-                    Console.ForegroundColor = ConsoleColor.Magenta;
-
-                    Console.WriteLine("Tamaño del lado #1:");
-                    double side_1 = 0;
-                    while (!double.TryParse(Console.ReadLine(), out side_1))
-                    {
-                        Console.WriteLine("Por favor ingrese un número real...");
-                    }
-                    Console.WriteLine("Tamaño del lado #2:");
-                    double side_2 = 0;
-                    while (!double.TryParse(Console.ReadLine(), out side_2))
-                    {
-                        Console.WriteLine("Por favor ingrese un número real...");
-                    }
-                    
-                    Console.Clear();
-                    Console.ForegroundColor = ConsoleColor.Green;
-
-                    if (side_1 == side_2)
-                    {
-                        cuadrado(side_1, side_2);
-                    }
-                    else
-                    {
-                        rectangulo(side_1, side_2);
-                    }
-                } else
+                    lados = integra_lados(type);
+                }
+                else
                 {
-                    Console.WriteLine("RESUELVA PARA UN POLÍGONO DE TRES LADOS");
-
-                    Console.ForegroundColor = ConsoleColor.Yellow;
-
-                    Console.WriteLine("Tamaño del lado #1:");
-                    double side_1 = 0;
-                    while (!double.TryParse(Console.ReadLine(), out side_1))
-                    {
-                        Console.WriteLine("Por favor ingrese un número real...");
-                    }
-                    Console.WriteLine("Tamaño del lado #2:");
-                    double side_2 = 0;
-                    while (!double.TryParse(Console.ReadLine(), out side_2))
-                    {
-                        Console.WriteLine("Por favor ingrese un número real...");
-                    }
-                    Console.WriteLine("Tamaño del lado #3:");
-                    double side_3 = 0;
-                    while (!double.TryParse(Console.ReadLine(), out side_3))
+                    Console.WriteLine("Lado de la figura regular:");
+                    while (!double.TryParse(Console.ReadLine(), out lado_normal))
                     {
                         Console.WriteLine("Por favor ingrese un número real...");
                     }
 
-                    Console.Clear();
-                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Apotema:");
+                    while (!double.TryParse(Console.ReadLine(), out apotema))
+                    {
+                        Console.WriteLine("Por favor ingrese un número real...");
+                    }
+                } 
 
-                    if (side_1 == side_2 && side_1 == side_3)
-                    {
-                        equilatero(side_1, side_2, side_3);
-                    }
-                    else if (side_1 == side_2 && side_1 != side_3)
-                    {
-                        isosceles(side_1, side_2, side_3);
-                    }
-                    else
-                    {
-                        escaleno(side_1, side_2, side_3);
-                    }
+                Console.Clear();
+                Console.ForegroundColor = ConsoleColor.Green;
+                switch (type)
+                {
+                    case 2:
+                        if (lados[0] == lados[1])
+                        {
+                            cuadrado(lados[0], lados[1]);
+                        }
+                        else
+                        {
+                            rectangulo(lados[0], lados[1]);
+                        }
+                        break;
+                    case 3:
+                        if (lados[0] == lados[1] && lados[0] == lados[2])
+                        {
+                            equilatero(lados[0], lados[1], lados[2]);
+                        }
+                        else if (lados[0] == lados[1] && lados[0] != lados[2])
+                        {
+                            isosceles(lados[0], lados[1], lados[2]);
+                        }
+                        else
+                        {
+                            escaleno(lados[0], lados[1], lados[2]);
+                        }
+                        break;
+                    case 4:
+                        trapezio(lados);
+                        break;
+                    case 5:
+                        pentagono(lado_normal);
+                        break;
+                    case 6:
+                        hexagono(lado_normal, apotema);
+                        break;
+                    case 7:
+                        heptagono(lado_normal, apotema);
+                        break;
+                    case 8:
+                        octagono(lado_normal, apotema);
+                        break;
                 }
 
                 Console.WriteLine("\nPULSE CUALQUIER TECLA PARA CONTINUAR...");
@@ -104,6 +103,25 @@ namespace MP___group_solution
                     return;
                 }
             }
+        }
+
+        static List<double> integra_lados(int numero_de_lados)
+        {
+            List<double> lados = new List<double>();
+
+            for (int i = 0; i < numero_de_lados; i++)
+            {
+                Console.WriteLine("Tamaño del lado #{0}:", i + 1);
+                double side = 0;
+                while (!double.TryParse(Console.ReadLine(), out side))
+                {
+                    Console.WriteLine("Por favor ingrese un número real...");
+                }
+
+                lados.Add(side);
+            }
+
+            return lados;
         }
 
         static void cuadrado(double a, double b)
@@ -156,6 +174,63 @@ namespace MP___group_solution
 
             double p = (a + b + c) / 2;
             Console.WriteLine("Superficie: {0}\nPerímetro: {1}", Math.Sqrt(p * (p - a) * (p - b) * (p - c)), a + b + c);
+        }
+        
+        static void pentagono(double a)
+        {
+            Console.WriteLine("CUADRADO POLÍGONO");
+            Console.WriteLine("-----------------");
+
+            double h = a / 1.45;
+
+            Console.WriteLine("Superficie: {0}\nPerímetro: {1}", ((a * 5) * h) / 2, a * 5);
+        }
+
+        static void trapezio(List<double> lados)
+        {
+            Console.WriteLine("POLIGONO TRAPEZOIDE");
+            Console.WriteLine("-------------------");
+
+            //A = Base MAYOR B = Base Menor C = Lado 
+            double a = lados.Max();
+            double b = lados.Min();
+            lados.Remove(a);
+            lados.Remove(b);
+            double c = lados.Max();
+
+            double n = (a - b) / 2;
+            double h = Math.Pow(c, 2) - Math.Pow(n, 2);
+            h = Math.Pow(h, (1 / 2));
+            double area = (a + b * h) / 2;
+
+            Console.WriteLine("Superficie: {0}\nPerimetro: {1}", area, (a + b + (c * 2)));
+        }
+
+        static void hexagono(double a, double apotema)
+        {
+            Console.WriteLine("HEXAGONO");
+            Console.WriteLine("-----------------");
+
+            double perimetro = 6 * a;
+            Console.WriteLine("Superficie: {0}\nPerimetro: {1}", ((perimetro * apotema) / 2), perimetro);
+
+        }
+
+        static void heptagono(double a, double apotema)
+        {
+            Console.WriteLine("HEPTÁGONO");
+            Console.WriteLine("-----------------");
+
+            double p = a * 7;
+            Console.WriteLine("Superficie: {0}\nPerímetro: {1}", (p * apotema) / 2, p);
+        }
+
+        static void octagono(double a, double apotema)
+        {
+            Console.WriteLine("OCTAGONO");
+            Console.WriteLine("-----------------");
+
+            Console.WriteLine("Superficie: {0}\nPerimetro: {1}", (4 * a * apotema), (8 * a));
         }
     }
 }
