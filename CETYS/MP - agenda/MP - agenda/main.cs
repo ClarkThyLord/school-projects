4da81558a282f31e4d976c084abf3e2f0151197f
Christian Cuevas <ccpotential@gmail.com>
2018-10-15 15:04:38 -0700

    finished
    Clark was here ;V

--- CETYS/MP - agenda/MP - agenda/main.cs
+++ CETYS/MP - agenda/MP - agenda/main.cs
@@ -37,6 +37,9 @@ namespace MP___agenda
                     case "r":
                         quitar();
                         break;
+                    case "l":
+                        lista();
+                        break;
                     case "s":
                         buscar();
                         break;
@@ -67,6 +70,7 @@ namespace MP___agenda
             Console.WriteLine("");
             Console.WriteLine("a       :   añadir personas a la agenda");
             Console.WriteLine("r       :   remover a la gente a la agenda");
+            Console.WriteLine("l       :   listar contactos de la agenda");
             Console.WriteLine("s       :   buscar personas en la agenda");
             Console.WriteLine("");
             Console.WriteLine("salir   :   salir del programa");
@@ -95,7 +99,7 @@ namespace MP___agenda
                 valor = Console.ReadLine();
 
                 Console.WriteLine("{0} ¿estás seguro? (y/n)", valor);
-                string ans = Console.ReadLine();
+                string ans = Console.ReadLine().ToLower();
                 if (!(ans == "n"))
                 {
                     break;
@@ -112,7 +116,7 @@ namespace MP___agenda
                 valor = Console.ReadLine();
 
                 Console.WriteLine("{0} ¿estás seguro? (y/n)", valor);
-                string ans = Console.ReadLine();
+                string ans = Console.ReadLine().ToLower();
                 if (!(ans == "n"))
                 {
                     break;
@@ -129,7 +133,7 @@ namespace MP___agenda
                 valor = Console.ReadLine();
 
                 Console.WriteLine("{0} ¿estás seguro? (y/n)", valor);
-                string ans = Console.ReadLine();
+                string ans = Console.ReadLine().ToLower();
                 if (!(ans == "n"))
                 {
                     break;
@@ -146,7 +150,7 @@ namespace MP___agenda
                 valor = Console.ReadLine();
 
                 Console.WriteLine("{0} ¿estás seguro? (y/n)", valor);
-                string ans = Console.ReadLine();
+                string ans = Console.ReadLine().ToLower();
                 if (!(ans == "n"))
                 {
                     break;
@@ -161,6 +165,8 @@ namespace MP___agenda
             Console.WriteLine("Telefono de Casa: {0}", db[_id].telefono_casa);
             Console.WriteLine("Telefono Celular: {0}", db[_id].telefono_celular);
             Console.WriteLine("Correo Electronico: {0}", db[_id].correo_electronico);
+
+            _id++;
         }
 
         static void quitar()
@@ -183,25 +189,53 @@ namespace MP___agenda
                     Console.WriteLine("Por favor ingrese un ID real, entre 0-99...");
                 }
 
-                if ((id < 0 && id > 99) || db[id].id != -1)
+                if (id < 0 || id > 99 || !(db[id] is MP___agenda.database.Contact))
                 {
-                    Console.WriteLine("No es un ID válido...");
+                    Console.WriteLine("No es un ID válido, ingrese un ID real...");
                     continue;
                 }
                 break;
             }
 
-            if (db[id].id != -1)
+            Console.WriteLine("{0} ~ {1} ¿Eliminar? (y/n)", id, db[id].nombre);
+            string ans = Console.ReadLine().ToLower();
+            if (ans == "n")
             {
-                Console.WriteLine("{0} ~ {1} ¿estás seguro? (y/n)", id, db[id].nombre);
-                string ans = Console.ReadLine();
-                if (!(ans == "n"))
-                {
-                    Console.WriteLine("Volviendo al menú principal ...");
-                    return;
-                }
+                Console.WriteLine("Volviendo al menú principal...");
+                return;
+            }
 
-                db[id].removed = false;
+            db[id].removed = true;
+
+            Console.WriteLine("Eliminado el contacto...");
+        }
+
+        static void lista()
+        {
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Gray;
+
+            if (_id == 0)
+            {
+                Console.WriteLine("No hay contactos para listar...");
+                return;
+            }
+
+            Console.WriteLine("******************");
+            Console.WriteLine("Lista de contactos");
+            Console.WriteLine("******************");
+
+            foreach (MP___agenda.database.Contact contact in db)
+            {
+                if (contact is MP___agenda.database.Contact && !contact.removed)
+                {
+                    Console.WriteLine("ID: {0}", contact.id);
+                    Console.WriteLine("Nombre: {0}", contact.nombre);
+                    Console.WriteLine("Telefono de Casa: {0}", contact.telefono_casa);
+                    Console.WriteLine("Telefono Celular: {0}", contact.telefono_celular);
+                    Console.WriteLine("Correo Electronico: {0}", contact.correo_electronico);
+                    Console.WriteLine("-----");
+                }
             }
         }
 
@@ -210,6 +244,86 @@ namespace MP___agenda
             Console.Clear();
             Console.ForegroundColor = ConsoleColor.Magenta;
 
+            if (_id == 0)
+            {
+                Console.WriteLine("No hay contactos para hacer una búsqueda...");
+                return;
+            }
+
+            Console.WriteLine("¿Qué te gustaría filtrar por? (1 : identificación, 2 : nombre, 3 : número de teléfono)");
+            int filter = 0;
+            while (true)
+            {
+                while (!int.TryParse(Console.ReadLine(), out filter))
+                {
+                    Console.WriteLine("No es un filtrar válido...\nIngrese: 1 : identificación, 2 : nombre, 3 : número de teléfono");
+                }
+
+                if (filter < 1 || filter > 3)
+                {
+                    Console.WriteLine("No es un filtrar válido...\nIngrese: 1 : identificación, 2 : nombre, 3 : número de teléfono");
+                    continue;
+                }
+                break;
+            }
+
+            bool found = false;
+            Console.WriteLine("Insertar término de búsqueda:");
+            if (filter == 1)
+            {
+                int id = 0;
+                while (true)
+                {
+                    while (!int.TryParse(Console.ReadLine(), out id))
+                    {
+                        Console.WriteLine("Por favor ingrese un ID real, entre 0-99...");
+                    }
+
+                    if (id < 0 || id > 99)
+                    {
+                        Console.WriteLine("No es un ID válido, entre 0-99...");
+                        continue;
+                    }
+                    break;
+                }
+
+                if (db[id] is MP___agenda.database.Contact && !db[id].removed)
+                {
+                    found = true;
+                    Console.WriteLine("ID: {0}", db[id].id);
+                    Console.WriteLine("Nombre: {0}", db[id].nombre);
+                    Console.WriteLine("Telefono de Casa: {0}", db[id].telefono_casa);
+                    Console.WriteLine("Telefono Celular: {0}", db[id].telefono_celular);
+                    Console.WriteLine("Correo Electronico: {0}", db[id].correo_electronico);
+                }
+            }
+            else
+            {
+                string key = Console.ReadLine().ToLower();
+                foreach (MP___agenda.database.Contact contact in db)
+                {
+                    if (contact is MP___agenda.database.Contact && !contact.removed)
+                    {
+                        if ((filter == 2 && !contact.nombre.ToLower().Contains(key)) || (filter == 3 && !contact.telefono_celular.ToLower().Contains(key)))
+                        {
+                            continue;
+                        }
+
+                        found = true;
+                        Console.WriteLine("ID: {0}", contact.id);
+                        Console.WriteLine("Nombre: {0}", contact.nombre);
+                        Console.WriteLine("Telefono de Casa: {0}", contact.telefono_casa);
+                        Console.WriteLine("Telefono Celular: {0}", contact.telefono_celular);
+                        Console.WriteLine("Correo Electronico: {0}", contact.correo_electronico);
+                        Console.WriteLine("-----");
+                    }
+                }
+            }
+
+            if (found == false)
+            {
+                Console.WriteLine("No encontré nada ...");
+            }
         }
     }
 }
