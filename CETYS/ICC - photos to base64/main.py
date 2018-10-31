a2657667ab4489b8bbce11f42f3641aa29889a01
Christian Cuevas <ccpotential@gmail.com>
2018-10-30 23:07:37 -0700

    export web
    Clark was here :V

--- CETYS/ICC - photos to base64/main.py
+++ CETYS/ICC - photos to base64/main.py
@@ -4,13 +4,16 @@ import json
 
 data = []
 
+print('Export for WEB? (y, n)')
+ans = input()
+
 for file_name in os.listdir('./C1'):
     print('./C1/' + file_name)
     with open('./C1/' + file_name, 'rb') as image_file:
         encoded_string = base64.b64encode(image_file.read())
 
         data.append({
-            'img': 'data:image/jpeg;base64,' + encoded_string.decode(),
+            'img': ('data:image/jpeg;base64,' if ans == 'y' else '') + encoded_string.decode(),
             'name': file_name.split('.')[0]
         })
 
