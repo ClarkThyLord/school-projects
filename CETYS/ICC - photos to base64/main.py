d0ab85a43a38a945d9326d3b60dcc1b9cebf3e7b
Christian Cuevas <ccpotential@gmail.com>
2018-10-30 22:48:24 -0700

    script
    Clark was here :V

---
+++ CETYS/ICC - photos to base64/main.py
@@ -0,0 +1,18 @@
+import os
+import base64
+import json
+
+data = []
+
+for file_name in os.listdir('./C1'):
+    print('./C1/' + file_name)
+    with open('./C1/' + file_name, 'rb') as image_file:
+        encoded_string = base64.b64encode(image_file.read())
+
+        data.append({
+            'img': encoded_string.decode(),
+            'name': file_name.split('.')[0]
+        })
+
+with open('./data.json', 'w') as out_file:
+    json.dump(data, out_file)
