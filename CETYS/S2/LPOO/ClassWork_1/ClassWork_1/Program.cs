d7ac2f6f04c956aabb55281ad53f661f34dfd4d9
Christian Cuevas <ccpotential@gmail.com>
2019-01-29 18:14:19 -0800

    class work 1
    Clark was here :V

---
+++ CETYS/S2/LPOO/ClassWork_1/ClassWork_1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassWork_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        static void work_1 ()
+        {
+            float SALDO = 0;
+            while (float.TryParse(Console.ReadLine(), out SALDO))
+            {
+                Console.WriteLine("¡El saldo no es válido!");
+            }
+        }
+    }
+}
