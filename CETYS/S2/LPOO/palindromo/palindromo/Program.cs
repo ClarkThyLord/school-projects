1010a4d1a82d87ab053ce1e4e0761ea5e00fce7d
Christian Cuevas <ccpotential@gmail.com>
2019-02-22 11:21:51 -0800

    old class work
    Clark was here :V

---
+++ CETYS/S2/LPOO/palindromo/palindromo/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace palindromo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(problem("racecar"));
+            Console.WriteLine(problem("mam"));
+            Console.ReadKey();
+        }
+
+        static bool problem(string text, string reversed="", int index=0, bool recursive = false)
+        {
+            bool result = true;
+
+            if (reversed == "")
+            {
+                char[] treversed = text.ToArray();
+                Array.Reverse(treversed);
+                reversed = string.Concat(treversed);
+            }
+            
+            if (text.Length <= 0 || index == text.Length || text[index] != reversed[index]) result = false;
+
+            if (result && index < text.Length) problem(text, reversed, index + 1, true);
+
+            return result;
+        }
+    }
+}
