573ab7dbf34058c89fd310d1428e508efb8d7d0d
Christian Cuevas <ccpotential@gmail.com>
2019-02-26 12:03:50 -0800

    classwork
    Clark was here :V

---
+++ CETYS/S2/LPOO/inheritance/inheritance/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace inheritance
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<person> people = new List<person>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        person add_person(string name = "", string first_name = "", string last_name = "", DateTime date_of_birth = new DateTime())
+        {
+            person _person = new person(name, first_name, last_name, date_of_birth);
+
+            people.Add(_person);
+
+            return _person;
+        }
+    }
+}
---
+++ CETYS/S2/LPOO/inheritance/inheritance/person.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inheritance
+{
+    class person
+    {
+        public string name;
+        public string first_name;
+        public string last_name;
+
+        public DateTime date_of_birth;
+
+        public person (string name, string first_name, string last_name, DateTime date_of_birth)
+        {
+            this.name = name;
+            this.first_name = first_name;
+            this.last_name = last_name;
+
+            this.date_of_birth = date_of_birth;
+        }
+
+        public string get_full_name()
+        {
+            return $"{name} {first_name} {last_name}";
+        }
+
+        public int get_age()
+        {
+            return DateTime.Now.Year - date_of_birth.Year;
+        }
+    }
+}
