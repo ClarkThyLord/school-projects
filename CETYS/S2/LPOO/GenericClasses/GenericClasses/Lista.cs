3edacbb7dac1ea6368407a5c8b16ba563e951156
Christian Cuevas <ccpotential@gmail.com>
2019-04-02 19:23:37 -0700

    generic classes
    Clark was here :V

---
+++ CETYS/S2/LPOO/GenericClasses/GenericClasses/Lista.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericClasses
+{
+    class Lista<T>
+    {
+       public Nodo<T> raiz { get; set; }
+
+        public int tamanio { get; private set; } = 0;
+       
+        
+        public void AgregarNodo(Nodo<T> nuevonodo)
+        {
+            if (raiz is null)
+            {
+                raiz = nuevonodo;
+            }
+            else
+            {
+                Nodo<T> temp = raiz;
+                while (!(temp.siguiente is null))
+                {
+                    temp = temp.siguiente;
+                }
+                temp.siguiente = nuevonodo;
+                nuevonodo.anterior = temp;
+            }
+            tamanio++;
+            
+        }
+
+        public void AgregaNodo(T valor )
+        {
+            AgregarNodo(new Nodo<T>(valor));
+        }
+
+        public override string ToString()
+        {
+            string resultado = "";
+            Nodo<T> actual = raiz;
+            for (int i = 0; i < tamanio; i++)
+            {
+                resultado += actual.valor + " ";
+                actual = actual.siguiente;
+            }
+            return resultado;
+        }
+        public void insertar(T valor, int index)
+        {
+            Nodo<T> actual = raiz;
+            Nodo<T> temp = new Nodo<T>(valor);
+
+            for (int i = 0; i < tamanio; i++)
+            {
+                if (i == index)
+                {
+                    actual.anterior.siguiente = temp;
+                    temp.siguiente = actual;
+                    break;
+                }
+                actual = actual.siguiente;
+            }
+        }
+        public void pop(int index)
+        {
+            Nodo<T> actual = raiz;
+
+
+            for (int i = 0; i < tamanio; i++)
+            {
+                if (i == index)
+                {
+                    actual.anterior.siguiente = actual.siguiente;
+                    actual.siguiente.anterior = actual.anterior;
+                    break;
+                }
+                actual = actual.siguiente;
+            }
+        }
+
+        public void remove(T valor )
+        {
+            Nodo<T> actual = raiz;
+            for (int i = 0; i < tamanio; i++)
+            {
+                
+                if (actual.Equals(valor))
+                {
+                    actual.anterior.siguiente = actual.siguiente;
+                    actual.siguiente.anterior = actual.anterior;
+                    break;
+                }
+                actual = actual.siguiente;
+            }
+        }
+
+        public bool isEmpty()
+        {
+            return tamanio == 0;
+        }
+        
+        public int count(T valor)
+        {
+            int result = 0;
+
+            Nodo<T> actual = raiz;
+            for (int i = 0; i < tamanio; i++)
+            {
+                if (actual.Equals(valor)) result++;
+                actual = actual.siguiente;
+            }
+
+            return result;
+        }
+
+        public bool contains(T valor)
+        {
+            bool result = false;
+
+            Nodo<T> actual = raiz;
+            for (int i = 0; i < tamanio; i++)
+            {
+                if (actual.Equals(valor))
+                {
+                    result = true;
+                    break;
+                }
+                actual = actual.siguiente;
+            }
+
+            return result;
+        }
+
+        public void extends(T[] iterable)
+        {
+            foreach (var item in iterable)
+            {
+                AgregaNodo(item);
+            }
+        }
+
+        public void reverse()
+        {
+            raiz = last();
+            Nodo<T> actual = raiz;
+            Nodo<T> temp;
+
+            for (int i = 0; i < tamanio; i++)
+            {
+                temp = actual.siguiente;
+                actual.siguiente = actual.anterior;
+                actual.anterior = temp;
+            }
+        }
+        public Nodo<T> last()
+        {
+            Nodo<T> result = raiz;
+            for (int i = 0; i < tamanio; i++)
+            {
+                result = result.siguiente;
+            }
+            return result;
+        }
+        
+
+       
+    }
+}
---
+++ CETYS/S2/LPOO/GenericClasses/GenericClasses/Nodo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericClasses
+{
+    class Nodo<T>
+    {
+        public T valor { get; set; }
+        public Nodo<T> siguiente { get; set; }
+        public Nodo<T> anterior { get; set; }
+        public Nodo(T valor)
+        {
+            this.valor = valor;
+        }
+        public Nodo(T valor, Nodo<T> siguiente)
+        {
+            this.valor = valor;
+            this.siguiente = siguiente;
+        }
+        public Nodo(T valor, Nodo<T> siguiente, Nodo<T> anterior)
+        {
+            this.valor = valor;
+            this.siguiente = siguiente;
+            this.anterior = anterior;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return valor.Equals(obj);
+        }
+    }
+}
