5462e0dce54a5dc0dcf6a25c6b112d0b485b845f
Christian Cuevas <ccpotential@gmail.com>
2019-05-20 22:31:31 -0700

    finished?
    Clark was here :V

--- CETYS/S2/DISCRETAS/AllRoutesOfGivenLength/main.py
+++ CETYS/S2/DISCRETAS/AllRoutesOfGivenLength/main.py
@@ -1,3 +1,5 @@
+import math
+
 def text_to_input(text):
     num = ''
     nums = []
@@ -14,9 +16,53 @@ def text_to_input(text):
 
     return nums[0], nums[1], nums[2], nums[3:]
 
+def pretty_matrix(matrix):
+    pretty = ''
+    matrix_size = int(math.sqrt(len(matrix)))
+
+    for index in range(len(matrix)):
+        pretty += str(matrix[index])
+        if index % matrix_size + 1 != matrix_size:  pretty += ' '
+        if index % matrix_size + 1 == matrix_size and index != len(matrix) - 1:  pretty += '\n'
+    
+    return pretty
+
+def matrix_pow(matrix, pow = 2):
+    if pow <= 1: return matrix
+    
+    rows = []
+    columns = []
+    matrix_size = int(math.sqrt(len(matrix)))
+    for dimension in range(matrix_size):
+        rows.append([])
+        columns.append([])
+    
+    for index in range(len(matrix)):
+        rows[int(index / matrix_size)].append(matrix[index])
+        columns[int(index % matrix_size)].append(matrix[index])
+
+    result = []
+    for row in rows:
+        value = 0
+        for column in columns:
+            for index in range(len(rows)):
+                value += row[index] * column[index]
+            result.append(value)
+            value = 0
+
+    return matrix_pow(result, pow - 1)
+
 if __name__ == "__main__":
     file = open('./example.txt', 'r')
     start, end, length, matrix = text_to_input(file.read())
     file.close()
 
-    print('Start: ', start, 'End: ', end, 'Length: ', length, 'Matrix: ', matrix)
+    print('Start: ', start, 'End: ', end, 'Length: ', length, 'Matrix: ')
+    print(pretty_matrix(matrix))
+    print('---')
+
+    result = matrix_pow(matrix, length)
+
+    print('Amount: ', result[start * int(math.sqrt(len(matrix))) + end])
+    print(pretty_matrix(result))
+    
