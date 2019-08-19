6895bfee333e1997509e93c0921d5e9d6a516b3e
ClarkThyLord <ccpotential@gmail.com>
2019-08-18 22:42:38 -0700

    progress on recreate!

--- CETYS/S3/EDD/apuntadores/v2/List.cpp
+++ CETYS/S3/EDD/apuntadores/v2/List.cpp
@@ -1,8 +1,10 @@
+#include <vector>
 #include <iostream>
 
 template <class T>
 struct Node {
     T payload;
+    Node<T>* prev = nullptr;
     Node<T>* next = nullptr;
 
     public:
@@ -14,263 +16,101 @@ struct Node {
 
 template <class T>
 class List{
-    Node<T>* head;
+    int size = 0;
+    Node<T>* head = nullptr;
     
     public:
-        List()
+        List() {}
+
+        List(T payload) : List()
         {
-            head = nullptr;
+            insert(payload);
         }
-    
-        void insert(int payload)
+
+        template<size_t N>
+        List(T (&payloads)[N]) : List()
+        {
+            for (auto payload : payloads) insert(payload);
+        }
+
+        List(std::initializer_list<T> const &payloads) : List()
+        {
+            for (auto payload : payloads) insert(payload);
+        }
+
+        void insert(T payload)
         {
             if (head == nullptr) head = new Node<T>(payload);
-            else
-            {
-                for (Node<T>* current = head; true; )
-                {
-                    if (current -> next == nullptr)
-                    {
-                        current -> next = new Node<T>(payload);
-                        break;
-                    }
-                    else current = current -> next;
-                }
-            }
+            else for (Node<T>* current = head; true; current = current -> next) if (current -> next == nullptr) { current -> next = new Node<T>(payload); break; }
+            size++;
         } 
     
-        void erase(int payload)
+        void erase(T payload)
         {
-            if (head -> payload == payload)
-            {
-                Node<T>* past_head = head;
-                head = head -> next;
-                delete past_head;
-            }
-            else
-            {
-                for (Node<T>* current = head; current != nullptr; )
-                {
-                    if (current -> next == nullptr) break;
-                    if (current -> next -> payload == payload)
-                    {
-                        Node<T>* past_node = current -> next;
-                        current -> next = current -> next -> next;
-                        delete past_node;
-                    }
-                    else current = current -> next;
-                }   
-            }
         }
     
         void erasem()
         {
-            int index = size();
-            if (index == 1 || index == 2)
-            {
-                Node<T>* past_head = head;
-                head = head -> next;
-                delete past_head;
-            }
-            else if (index > 2)
-            {
-                index = index / 2;
-                int position = 0;
-                for (Node<T>* current = head; current != nullptr; position++)
-                {
-                    if (position + 1 == index && current -> next != nullptr)
-                    {
-                        Node<T>* past_node = current -> next;
-                        current -> next = current -> next -> next;
-                        delete past_node;
-                        return;
-                    }
-                    else if (position > index) break;
-                    current = current -> next;
-                }
-            }
+        }
+
+        T search(T payload)
+        {
+        }
+
+        T get(int index)
+        {
         }
     
-        int size()
+        T rget(int index)
         {
-            int size = 0;
-            for (Node<T>* current = head; current != nullptr; size++) current = current -> next;
-            return size;
         }
 
-        Node<T>* search(int payload)
+        T minimum()
         {
-            for (Node<T>* current = head; current != nullptr; )
-            {
-                if (current -> payload == payload) return current;
-                current = current -> next;
-            }
-            return nullptr;
         }
 
-        int get(int index)
-        {
-            int position = 0;
-            for (Node<T>* current = head; current != nullptr; position++)
-            {
-                if (position == index) return current -> payload;
-                current = current -> next;
-            }
-            return 0;
-        }
-    
-        int rget(int index)
-        {
-            index = size() - index - 1;
-            int position = 0;
-            for (Node<T>* current = head; current != nullptr; ++position)
-            {
-                if (position == index) return current -> payload;
-                current = current -> next;
-            }
-            return 0;
-        }
-
-        int minimum()
+        T maximum()
         {
             int res = 0;
-            if (head != nullptr)
-            {
-                res = head -> payload;
-                for (Node<T>* current = head -> next; current != nullptr; )
-                {
-                    if (current -> payload < res) res = current -> payload;
-                    current = current -> next;
-                }
-            }
-            return res;
         }
 
-        int maximum()
+        Node<T>* succesor(T payload)
         {
-            int res = 0;
-            if (head != nullptr)
-            {
-                res = head -> payload;
-                for (Node<T>* current = head -> next; current != nullptr; )
-                {
-                    if (current -> payload > res) res = current -> payload;
-                    current = current -> next;
-                }
-            }
-            return res;
-        }
-
-        Node<T>* succesor(int payload)
-        {
-            for (Node<T>* current = head; current != nullptr; )
-            {
-                if (current -> payload == payload) return current -> next;
-                else current = current -> next;
-            }
-            return nullptr;
         }
 
         Node<T>* predecessor(int payload)
         {
-            for (Node<T>* current = head; current != nullptr; )
-            {
-                if (current -> next == nullptr) break;
-                else if (current -> next -> payload == payload) return current;
-                else current = current -> next;
-            }
-            return nullptr;
         }
     
         void unique()
         {
-            if (head != nullptr)
-            {
-                List valids;
-                valids.insert(head -> payload);
-                for (Node<T>* current = head; current != nullptr; )
-                {
-                    if (current -> next == nullptr) break;
-                    else if (valids.search(current -> next -> payload) == nullptr) valids.insert(current -> next -> payload);
-                    else
-                    {
-                        Node<T>* past_node = current -> next;
-                        current -> next = current -> next -> next;
-                        delete past_node;
-                    }
-                    current = current -> next;
-                }
-            }
         }
     
-        void split(int value, List *min_list, List *max_list)
+        void split(T value, List *min_list, List *max_list)
         {
-            for (Node<T>* current = head; current != nullptr; )
-            {
-                if (current -> payload < value) min_list -> insert(current -> payload);
-                else max_list -> insert(current -> payload);
-                current = current -> next;
-            }
         }
     
         void print()
         {
-            for (Node<T>* current = head; current != nullptr; )
-            {
-                std::cout << current -> payload << (current -> next == nullptr ? "" : ", ");
-                current = current -> next;
-            }
+            for (Node<T>* current = head; current != nullptr; current = current -> next) std::cout << current -> payload << (current -> next == nullptr ? "" : ", ");
         }
 };
 
 int main(void)
 {
     List<int> test;
-    test.insert(7);
-    test.insert(8);
-    test.insert(-1);
-    std::cout << test.search(7) -> payload << '\n';
-    std::cout << test.search(8) -> payload << '\n';
-    test.erase(8);
-    std::cout << test.search(8) << '\n';
-    test.insert(-11);
-    test.insert(12);
-    test.insert(402);
-    std::cout << test.minimum() << '\n';
-    std::cout << test.maximum() << '\n';
-    std::cout << test.predecessor(-1) -> payload << '\n';
-    std::cout << test.succesor(-1) -> payload << '\n';
-    std::cout << test.size() << '\n';
-    std::cout << test.get(0) << '\n';
-    std::cout << test.get(2) << '\n';
-    std::cout << test.get(6) << '\n';
-    std::cout << test.rget(0) << '\n';
-    std::cout << test.rget(2) << '\n';
-    std::cout << test.rget(6) << '\n';
-    test.print();
-    std::cout << '\n';
-    test.erasem();
-    test.print();
-    std::cout << '\n';
-    List<int>* mins = new List<int>();
-    List<int>* maxs = new List<int>();
-    test.split(0, mins, maxs);
-    mins -> print();
-    std::cout << '\n';
-    maxs -> print();
-    std::cout << '\n';
     test.insert(1);
+    test.insert(2);
     test.insert(3);
-    test.insert(4);
-    test.insert(-9);
-    test.insert(-11);
-    test.insert(12);
-    test.insert(12);
-    test.insert(-2);
-    test.insert(4);
+    
+    // List<int> test = *new List<int>(1);
+    // test.insert(2);
+    // test.insert(3);
+
+    // int payloads[] = { 1, 2, 3 };
+    // List<int> test = *new List<int>(payloads);
+    
+    // List<int> test = *new List<int>({ 1, 2, 3});
+    
     test.print();
-    std::cout << '\n';
-    test.unique();
-    test.print();
-    std::cout << '\n';
 }
