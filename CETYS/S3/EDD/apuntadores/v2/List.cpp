9410a814546a0f03a775aaf1b0a95a50699304ac
ClarkThyLord <ccpotential@gmail.com>
2019-08-18 23:48:01 -0700

    more progress!

--- CETYS/S3/EDD/apuntadores/v2/List.cpp
+++ CETYS/S3/EDD/apuntadores/v2/List.cpp
@@ -19,6 +19,30 @@ class List{
     int size = 0;
     Node<T>* head = nullptr;
     
+    private:
+        Node<T>* getN(int index)
+        {
+            if (abs(index) >= size) throw std::invalid_argument("index out of bound");
+            else if (index < 0) index = size + index;
+            Node<T>* current = head;
+            for (int i = 0; i < index; i++) current = current -> next;
+            return current;
+        }
+
+        Node<T>* searchN(T payload)
+        {
+            for (Node<T>* current = head; current != nullptr; current = current -> next) if (current -> payload == payload) return current;
+        }
+
+        void eraseN(Node<T>* node)
+        {
+            if (node -> prev == nullptr) head = node -> next;
+            else node -> prev -> next = node -> next;
+            if (node -> next != nullptr) node -> next -> prev = node -> prev;
+            delete node;
+            size--;
+        }
+
     public:
         List() {}
 
@@ -38,31 +62,42 @@ class List{
             for (auto payload : payloads) insert(payload);
         }
 
+        T get(int index)
+        {
+            return getN(index) -> payload;
+        }
+
+        T search(T payload)
+        {
+            return searchN(payload) -> payload;
+        }
+
         void insert(T payload)
         {
             if (head == nullptr) head = new Node<T>(payload);
-            else for (Node<T>* current = head; true; current = current -> next) if (current -> next == nullptr) { current -> next = new Node<T>(payload); break; }
+            else for (Node<T>* current = head; true; current = current -> next) if (current -> next == nullptr)
+            {
+                current -> next = new Node<T>(payload);
+                current -> next -> prev = current;
+                break;
+            }
             size++;
         } 
     
         void erase(T payload)
         {
+            eraseN(searchN(payload));
         }
     
         void erasem()
         {
-        }
-
-        T search(T payload)
-        {
-        }
-
-        T get(int index)
-        {
-        }
-    
-        T rget(int index)
-        {
+            if (size == 1 || size == 2) eraseN(head);
+            else if (size > 2)
+            {
+                Node<T>* current = head;
+                for (int i = 0; i < size / 2; i++) current = current -> next;
+                eraseN(current);
+            }
         }
 
         T minimum()
@@ -113,4 +148,34 @@ int main(void)
     // List<int> test = *new List<int>({ 1, 2, 3});
     
     test.print();
+    std::cout << "\n---\n";
+
+
+    test.erase(1);
+    // test.erase(2);
+    // test.erase(3);
+
+    test.print();
+    std::cout << "\n---\n";
+
+
+    test.insert(7);
+    test.insert(4);
+    test.insert(5);
+    test.erasem();
+
+    test.print();
+    std::cout << "\n---\n";
+
+
+    std::cout << test.get(0) << ", ";
+    std::cout << test.get(1) << ", ";
+    std::cout << test.get(2) << ", ";
+    std::cout << test.get(3) << ", ";
+    try { std::cout << test.get(4); } catch(const std::exception& e) { std::cout << e.what() << ", "; }
+    std::cout << test.get(-1);
+
+    std::cout << '\n';
+    test.print();
+    std::cout << "\n---\n";
 }
