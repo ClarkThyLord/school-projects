87e8da35b5de49d16e9fa00df5f12cbbf72f45dd
Christian Cuevas <ccpotential@gmail.com>
2019-09-02 19:46:57 -0700

    fixed up Act#2
    Clark was here :V

--- CETYS/S3/MN/Actividad #2/main.py
+++ CETYS/S3/MN/Actividad #2/main.py
@@ -11,32 +11,32 @@ def main():
             '4    - Bisection  : sen(x) - x^2\n',
             '5    - Newton     : sen(x) - x^2\n',
             '6    - Punto Fijo : sen(x) - x^2\n',
-            '7    - Punto Fijo : sec(x) -> π',
+            '7    - Newton : sec(x) -> π',
             'exit - exit the program'
         )
         ans = input()
         if ans == '1': print(bisection(func_1, 0, 1, 100))
-        elif ans == '2': print(newton(func_2, func_2d, 0, 100))
-        elif ans == '3': print('')
+        elif ans == '2': print(newton(func_2, func_2d, 0.0001, 100))
+        elif ans == '3': print(fixed_point(func_2c, 1))
         elif ans == '4': print(bisection(func_3, 0, 1, 100))
         elif ans == '5': print(newton(func_3, func_3d, 100))
-        elif ans == '6': print(fixed_point(func_3, func_3c))
-        elif ans == '7': print('')
+        elif ans == '6': print(fixed_point(func_3c))
+        elif ans == '7': print(bisection(func_4, 0, 4))
         elif ans == 'exit': return
         else: print('invalid input')
 
 
 def func_1(x): return math.pow(x, 3) - 3 * math.pow(x, 2) + 1
 
-def func_2(x): return 21 ** 3
-def func_2d(x): return 0
+def func_2(x): return x ** 3 - 21
+def func_2d(x): return 3 * x ** 2
+def func_2c(x): return math.sqrt(21 / x)
 
 def func_3(x): return math.sin(x) - math.pow(x, 2)
 def func_3d(x): return math.cos(x) - 2 * x
 def func_3c(x): return math.sin(x) / x
 
-def func_4(x): return sen(x)
-def func_4c(x): return 0
+def func_4(x): return math.sin(x)
 
 
 def sign(x): return 1 if x >= 0 else 0
@@ -61,8 +61,8 @@ def newton(f, fd, x0, iterations=100):
         x0 = x1
     return x1
 
-def fixed_point(f, fc, x=0.5, iterations=100):
-    x1 = 0
+def fixed_point(fc, x=0.5, iterations=100):
+    x1 = x
     for i in range(iterations):
         x1 = fc(x1)
     return x1
