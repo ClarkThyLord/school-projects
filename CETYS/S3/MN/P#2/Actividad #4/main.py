647f21bbb3acac396347b621cad0d29fb95e6832
Christian Cuevas <ccpotential@gmail.com>
2019-10-15 22:18:19 -0700

    finished activity #4
    Clark was here :V

--- CETYS/S3/MN/P#2/Actividad #4/main.py
+++ CETYS/S3/MN/P#2/Actividad #4/main.py
@@ -37,6 +37,35 @@ def gauss_seidel(A, b, max_iterations = 1000):
         print('Step : ', 1 + it_count, ' ~ ', x)
     return x.tolist(), np.dot(A, x) - b
 
+# https://en.wikipedia.org/wiki/LU_decomposition
+def LU(A, b):
+    A = np.array(A)
+    b = np.array(b)
+    L = np.tril(A)
+    U = np.triu(A)
+    
+    L[0][0] = 1
+    L[1][1] = 1
+    L[2][2] = 1
+
+    L[1][0] = A[1][0] / U[0][0]
+    U[1][1] = A[1][1] - (L[1][0] * U[0][1])
+    U[1][2] = A[1][2] - (L[1][0] * U[0][2])
+    
+    L[2][0] = A[2][0] / U[0][0]
+    L[2][1] = (A[2][1] - L[2][0] * U[0][1]) / U[1][1]
+    U[2][2] = A[2][2] - (L[2][0]*U[0][2] + L[2][1]*U[1][2])
+
+    x = b[0] / L[0][0]
+    y = (b[1] - L[1][0] * x) / (L[1][1])
+    z = (b[2] - L[2][0] * x - L[2][1] * y) / L[2][2]
+
+    z = (z / U[2][2])
+    y = (y - U[1][2] * z) / U[1][1]
+    x = (x - U[0][1] * y - U[0][2] * z) / U[0][0]
+
+    return (x, y, z)
+
 
 if __name__== "__main__":
     print('1-)')
@@ -55,3 +84,19 @@ if __name__== "__main__":
     print('Conclusion : Al ejecutar esto desde mi escritorio, obtengo un resultado más preciso al usar el método Jacobi, el método Gauss Seidel requiere más iteraciones para obtener un resultado exacto')
 
     print('\n\n2-)')
+    A = [
+        [10., 12., 15.],
+        [6., 8., 12.],
+        [12., 12., 18.]
+    ]
+    b = [960., 660., 1080.]
+    print('LU : ', LU(A, b))
+
+    print('\n\n3-)')
+    A = [
+        [2., 3., 4.],
+        [1., 3., 2.],
+        [1., 3., 1.]
+    ]
+    b = np.array([90., 50., 40.])
+    print('LU : ', LU(A, b))
