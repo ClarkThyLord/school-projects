ad0ea10c9e4d92cec2021454f53b222be26de30f
Christian Cuevas <ccpotential@gmail.com>
2019-10-16 17:24:05 -0700

    fix up mflse exam p.1
    Clark was here :V

--- CETYS/S3/MN/P#2/Exam/main.py
+++ CETYS/S3/MN/P#2/Exam/main.py
@@ -37,42 +37,15 @@ def gauss_seidel(A, b, max_iterations = 1000):
         print('Step : ', 1 + it_count, ' ~ ', x)
     return x, np.dot(A, x) - b
 
-# https://en.wikipedia.org/wiki/LU_decomposition
-def LU(A, b):
-    A = np.array(A)
-    b = np.array(b)
-
-    L = np.tril(A)
-    U = np.triu(A)
-
-    for i in range(len(L) - 1): L[i][i] = 1
-
-    L[1][0] = A[1][0] / U[0][0]
-    U[1][1] = A[1][1] - (L[1][0] * U[0][1])
-    U[1][2] = A[1][2] - (L[1][0] * U[0][2])
-    L[2][0] = A[2][0] / U[0][0]
-    L[2][1] = (A[2][1] - L[2][0] * U[0][1]) / U[1][1]
-    U[2][2] = A[2][2] - (L[2][0]*U[0][2] + L[2][1]*U[1][2])
-
-    x = b[0] / L[0][0]
-    y = (b[1] - L[1][0] * x) / (L[1][1])
-    z = (b[2] - L[2][0] * x - L[2][1] * y) / L[2][2]
-
-    z = (z / U[2][2])
-    y = (y - U[1][2] * z) / U[1][1]
-    x = (x - U[0][1] * y - U[0][2] * z) / U[0][0]
-
-    return L, U, (x, y, z)
-
 
 if __name__ == "__main__":
     A = [
-            [10., -1., 2., 0.],
-            [-1., 11., -1., 3.],
-            [2., -1., 10., -1.],
-            [0., 3., -1., 8.]
-    ]
-    b = [6., 25., -11., 15.]
+            [4., -1., 1., 3.],
+            [-1., 5., 1., 1.],
+            [2., 0., 4., 2.],
+            [0., 1., -1., 7.]
+        ]
+    b = [14., -1., 19., 10.]
     
     result, error = jacobi(A, b)
     print('Jacobi ~ Result: ', result, 'Error: ', error)
@@ -80,11 +53,3 @@ if __name__ == "__main__":
     result, error = gauss_seidel(A, b)
     print('Gauss Seidel ~ Result: ', result, 'Error: ', error)
 
-    A = [
-            [10., -1., 2.],
-            [-1., 11., -1.],
-            [2., -1., 10.],
-            [0., 3., -1.]
-    ]
-    b = [6., 25., -11.]
-    print('LU : ', LU(A, b))
