4ff6c4a7cf529afedc1457fe2cdd139aeb0b7184
Christian Cuevas <ccpotential@gmail.com>
2019-10-19 23:50:03 -0700

    start Act #6
    Clark was here :V

---
+++ CETYS/S3/MN/P#2/Actividad #6/main.py
@@ -0,0 +1,37 @@
+import math
+import numpy as np
+
+
+# https://www.mathsisfun.com/data/least-squares-regression.html
+def least_squares_regression(points):
+    sum_x = 0.
+    sum_y = 0.
+    sum_x2 = 0.
+    sum_xy = 0.
+
+    for point in points:
+        sum_x += point[0]
+        sum_y += point[1]
+        sum_x2 += point[0] ** 2
+        sum_xy += point[0] * point[1]
+
+    m = ((len(points) * sum_xy) - (sum_x * sum_y)) / ((len(points) * sum_x2) - (sum_x ** 2))
+
+    b = (sum_y - (m * sum_x)) / len(points)
+
+    return m, b
+
+def least_squares_regression_func(m, b, x):
+    return m * x + b
+
+
+if __name__== "__main__":
+    print('1-)')
+    points = [
+        (0., 0.),
+    ]
+    m, b = least_squares_regression(points)
+    print('y =', m, '* x', '+', b)
+    for x in range(0, 14, 1):
+        print('Point : ', points[x], ' ~ LSR : ', least_squares_regression_func(m, b, x))
+    
