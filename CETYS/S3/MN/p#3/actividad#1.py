572060603b7affb1e20fa795baf1983049fbbc4d
Christian Cuevas <ccpotential@gmail.com>
2019-11-17 18:32:42 -0800

    fix text
    Clark was here :V

--- CETYS/S3/MN/p#3/actividad#1.py
+++ CETYS/S3/MN/p#3/actividad#1.py
@@ -42,14 +42,12 @@ if __name__ == '__main__':
     a = 1
     b = 2
     n = 12
-    print('1.b) (x+1/x)^2 [1, 2] n=6 ->\ntrapecio:   ', trapecio(f,a, b, n), '\nsimpson 1/3:', simpson_1_3(f, a, b, n),'\nsimpson 3/8:', simpson_3_8(f, a, b, n))
+    print('1.b) (x+1/x)^2 [1, 2] n=12 ->\ntrapecio:   ', trapecio(f,a, b, n), '\nsimpson 1/3:', simpson_1_3(f, a, b, n),'\nsimpson 3/8:', simpson_3_8(f, a, b, n))
 
     f = lambda x : (x ** 2) / math.sqrt(4 - (x ** 2))
     a = -1
     b = math.sqrt(3)
     n = 18
-    print('1.c) x^2 / sqrt(4 - x^2) [1, 2] n=6 ->\ntrapecio:   ', trapecio(f,a, b, n), '\nsimpson 1/3:', simpson_1_3(f, a, b, n),'\nsimpson 3/8:', simpson_3_8(f, a, b, n))
+    print('1.c) x^2 / sqrt(4 - x^2) [-1, sqrt(3)] n=18 ->\ntrapecio:   ', trapecio(f,a, b, n), '\nsimpson 1/3:', simpson_1_3(f, a, b, n),'\nsimpson 3/8:', simpson_3_8(f, a, b, n))
 
     print('============')
-
-    
