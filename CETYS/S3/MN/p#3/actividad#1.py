f73f8eb6c6f6ab6a5387822a333c2493313bd7fe
Christian Cuevas <ccpotential@gmail.com>
2019-11-17 18:29:52 -0800

    simpson_1_3

--- CETYS/S3/MN/p#3/actividad#1.py
+++ CETYS/S3/MN/p#3/actividad#1.py
@@ -1,21 +1,26 @@
 import math
 
+# h = (b - a) / n
 
 def trapecio(f, a, b, n):
-    x = 0
+    x = ((f(a) + f(b)) / 2)
 
     for k in range(n - 1):
         x += f(a + (k * ((b - a) / n)))
-
-    x += ((f(a) + f(b)) / 2)
     
     return ((b - a) / n) * x
 
 def simpson_1_3(f, a, b, n):
-    return 
+    x = f(a) + f(b)
+    
+    for k in range(n - 1):
+        step = a + k * ((b - a) / n)
+        x += 2 * f(step) if k % 2 == 0 else 4 * f(step)
+    
+    return x * (((b - a) / n) / 3)
 
 def simpson_3_8(f, a, b, n):
-    return 
+    return
 
 
 if __name__ == '__main__':
@@ -24,19 +29,19 @@ if __name__ == '__main__':
     f = lambda x : x * math.log(x)
     a = 1
     b = 2
-    n = 6
+    n = 10000
     print('1.a) xln(x) [1, 2] n=6 ->\ntrapecio:   ', trapecio(f,a, b, n), '\nsimpson 1/3:', simpson_1_3(f, a, b, n),'\nsimpson 3/8:', simpson_3_8(f, a, b, n))
 
     f = lambda x : (x + (2 / x)) ** 2
     a = 1
     b = 2
-    n = 12
+    n = 10000
     print('1.b) (x+1/x)^2 [1, 2] n=6 ->\ntrapecio:   ', trapecio(f,a, b, n), '\nsimpson 1/3:', simpson_1_3(f, a, b, n),'\nsimpson 3/8:', simpson_3_8(f, a, b, n))
 
     f = lambda x : (x ** 2) / math.sqrt(4 - (x ** 2))
     a = -1
     b = math.sqrt(3)
-    n = 18
+    n = 10000
     print('1.c) x^2 / sqrt(4 - x^2) [1, 2] n=6 ->\ntrapecio:   ', trapecio(f,a, b, n), '\nsimpson 1/3:', simpson_1_3(f, a, b, n),'\nsimpson 3/8:', simpson_3_8(f, a, b, n))
 
     print('============')
