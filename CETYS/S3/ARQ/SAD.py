91bdd89d59fbcc652497c41765c9b1c2a612c982
Christian Cuevas <ccpotential@gmail.com>
2019-12-02 22:15:35 -0800

    fix up
    Clark was here :V

--- CETYS/S3/ARQ/SAD.py
+++ CETYS/S3/ARQ/SAD.py
@@ -23,23 +23,28 @@ def save(file_path, data):
 def tokenize(source):
     return ''
 
+def compile(source_file_path, hex_file_path=''):
+    pass
+
 
 if __name__ == '__main__':
     while True:
-        print('*******************+')
-        print('SAD Compiler v0.0.1|')
-        print('===================+')
+        print('************************+')
+        print('SAD Compiler v0.0.1     |')
+        print('========================+')
         
         print('File to compile: (*.%s)' % FILE_EXTENSION)
-        file_path = input().replace('"', '').replace("'", '')
+        source_file_path = input().replace('"', '').replace("'", '')
         print('File...')
-        if not file_path.endswith('.' + FILE_EXTENSION):
+        if not source_file_path.endswith('.' + FILE_EXTENSION):
             print('INVALID')
-        elif os.path.exists(file_path):
-            print('FOUND')
+        elif os.path.exists(source_file_path):
+            print('FOUND\nCompiling...')
+            compile(source_file_path)
+            print('FINISHED')
         else: print('NOT FOUND')
         if input("Press Enter to continue, or enter \"exit\" to quit...\n").lower() == 'exit':
             print('Goodbye ;)')
-            print('********************')
+            print('*************************')
             exit()
         os.system('cls' if os.name == 'nt' else 'clear')
