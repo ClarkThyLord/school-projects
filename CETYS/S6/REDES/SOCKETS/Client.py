1e91ca5185986ad6bea1c7cec5dd4391ce7e1043
Christian Cuevas <ccpotential@gmail.com>
2021-04-17 10:30:24 -0700

    with hamachi

--- CETYS/S6/REDES/SOCKETS/Client.py
+++ CETYS/S6/REDES/SOCKETS/Client.py
@@ -7,8 +7,8 @@ HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
 PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
 
 # HAMACHI
-HOST = '25.83.165.88'  # The server's hostname or IP address
-PORT = 65432           # The port used by the server
+# HOST = '25.83.165.88'  # The server's hostname or IP address
+# PORT = 65432           # The port used by the server
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
