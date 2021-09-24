38d9ec72180291bc119481c746671a14acf5ccf7
Christian Cuevas <ccpotential@gmail.com>
2021-09-24 16:24:08 -0700

    Measure elapsed time

--- CETYS/S7/PARALLEL/serial_pis.cpp
+++ CETYS/S7/PARALLEL/serial_pis.cpp
@@ -1,5 +1,9 @@
 #include <omp.h>
-#include <stdio.h>
+#include <iostream>
+#include <chrono>
+#include <unistd.h>
+
+using namespace std;
 
 static int threads = 4;
 static long num_steps = 100'000'000;
@@ -8,6 +12,8 @@ double step;
 
 int main()
 {
+    auto start = chrono::steady_clock::now();
+
     double pi = 0.0;
     step = 1.0 / num_steps;
     int at = 0;
@@ -23,5 +29,14 @@ int main()
         pi += step * sum;
         at += range;
     }
-    printf("PI: %f\n", pi);
+
+    auto end = chrono::steady_clock::now();
+
+    cout << "PI: "
+         << pi << endl;
+    cout << "Elapsed Time: "
+         << chrono::duration_cast<chrono::seconds>(end - start).count()
+         << " sec / "
+         << chrono::duration_cast<chrono::milliseconds>(end - start).count()
+         << " ms" << endl;
 }
