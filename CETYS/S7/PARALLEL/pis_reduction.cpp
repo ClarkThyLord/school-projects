866f053af5c3dc1880b1afc1889d7d8f8a713231
Christian Cuevas <ccpotential@gmail.com>
2021-10-11 22:47:23 -0700

    Working Reduction

--- CETYS/S7/PARALLEL/pis_reduction.cpp
+++ CETYS/S7/PARALLEL/pis_reduction.cpp
@@ -1,67 +1,38 @@
 // To Compile:
 // g++ -fopenmp <file_path>
 
-#include <omp.h>
-#include <iostream>
 #include <chrono>
+#include <iostream>
 #include <unistd.h>
 
 using namespace std;
 
-#define PAD 8
-
 static long num_steps = 1'000'000'000;
+double step;
 
 int main()
 {
-    for (int THREADS = 1; THREADS < 7; THREADS++)
-    {
-        double pi = 0.0;
+    auto start = chrono::steady_clock::now();
 
-        auto start = chrono::steady_clock::now();
+    int i;
+    double x, pi, sum = 0.0;
 
-        pi = 0.0;
-
-        int i = 0;
-        int nthreads = 0;
-
-        double step = 1.0 / num_steps;
-
-        omp_set_num_threads(THREADS);
+    step = 1.0 / num_steps;
 #pragma omp parallel for reduction(+ \
                                    : pi)
-        {
-            int i = 0;
-            int id = 0;
-            int nthrds = 0;
-            double x = 0.0;
-            double sum = 0.0;
-
-            id = omp_get_thread_num();
-            nthrds = omp_get_num_threads();
-            if (id == 0)
-                nthreads = nthrds;
-            for (int i = id; i < num_steps; i = i + nthrds)
-            {
-                x = (i + 0.5) * step;
-                sum += (4.0 / (1.0 + x * x));
-            }
-
-            pi += sum * step;
-        }
-
-        auto end = chrono::steady_clock::now();
-
-        cout << "THREADS: "
-             << nthreads << " / " << THREADS << endl;
-        cout << "PI: "
-             << pi << endl;
-        cout << "Elapsed Time: "
-             << chrono::duration_cast<chrono::seconds>(end - start).count()
-             << " sec / "
-             << chrono::duration_cast<chrono::milliseconds>(end - start).count()
-             << " ms" << endl;
+    for (size_t i = 0; i < num_steps; i++)
+    {
+        x = (i + 0.5) * step;
+        pi += step * (4.0 / (1.0 + x * x));
     }
 
-    return 0;
+    auto end = chrono::steady_clock::now();
+
+    cout << "PI: "
+         << pi << endl;
+    cout << "Elapsed Time: "
+         << chrono::duration_cast<chrono::seconds>(end - start).count()
+         << " sec / "
+         << chrono::duration_cast<chrono::milliseconds>(end - start).count()
+         << " ms" << endl;
 }
